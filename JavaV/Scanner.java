e3955d13b6039a8d7dc26c169de5487d13aee1ff
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-08-14 20:51:19 +0200

    Add files via upload

--- JavaV/Scanner.java
+++ JavaV/Scanner.java
@@ -70,11 +70,53 @@ class Scanner {
         break;
         
         case '"': string(); break;
-        
+                }
         default:
+            if (isDigit(c)) {
+            number();
+        }
+        else if (isAlpha(c)) {
+            identifier();
+        }
+         else {
             Lox.error(line, "Unexpected character.");
-            break;
-         }
+        }
+        break;
+    }
+    
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+    
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+    
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
+    }
+    
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+    
+    private void number() {
+    while (isDigit(peek())) advance();
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+            while (isDigit(peek())) advance();
+        }
+        addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -84,7 +126,11 @@ class Scanner {
         }
     if (isAtEnd()) {
         Lox.error(line, "Unterminated string.");
-        return;
+        return;private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+    }
+    }private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
     // The closing ".
     advance();
@@ -104,6 +150,10 @@ class Scanner {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
     
     
     private boolean isAtEnd() {
@@ -122,6 +172,25 @@ class Scanner {
         tokens.add(new Token(type, text, literal, line));
     }
 
-    
+private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
 }
