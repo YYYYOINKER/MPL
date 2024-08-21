c2831a1c9047c2ed386bf3d16bdac116aa4f3c42
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-08-21 18:46:13 +0200

    Add files via upload

--- Java/src/com/craftinginterpreters/lox/Lox.java
+++ Java/src/com/craftinginterpreters/lox/Lox.java
@@ -48,10 +48,13 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        // Stop if there was a syntax error.
+        if (hadError) return;
+        System.out.println(new AstPrinter().print(expression));
 
-        for (Token token : tokens) {
-            System.out.println(tokens);
-        }
+        
     }
     
     static void error(int line, String message) {
@@ -61,5 +64,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line "+line+"] Error"+where+": "+message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+        report(token.line, " at end", message);
+        } else {
+        report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
